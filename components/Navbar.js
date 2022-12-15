8e1d383473a6cfab448dfd6df9703e48f43e020f
Sahil pillania <sahilpillania88@gmail.com>
2022-12-15 12:33:21 +0530

    Footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,37 @@
 import React from "react";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-col md:flex-row md:justify-start items-center justify-between align-middle p-3 shadow-md bg-slate-300">
+      <div className="logo font-medium">
+        <span className={styles.icon_name}>Dresses World</span>
+      </div>
+      <div className="nav mx-4">
+        <ul className="flex space-x-4 items-center font-medium">
+          <Link href={"/tshirts"}>
+            <li>Tshirts</li>
+          </Link>
+          <Link href={"/hoodies"}>
+            <li>Hoodies</li>
+          </Link>
+          <Link href={"/mugs"}>
+            <li>Mugs</li>
+          </Link>
+          <Link href={"/stickers"}>
+            <li>Stickers</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="cart absolute top-2 right-4 md:mx-4 my-3">
+        <div>
+          <AiOutlineShoppingCart className="text-2xl" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
