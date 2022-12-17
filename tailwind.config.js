294c0fc5b44598253bee593786baf51f00cd1654
Sahil pillania <sahilpillania88@gmail.com>
2022-12-17 11:14:26 +0530

    custom breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        ssm: "460px",
+      },
+    },
   },
   plugins: [],
 };
