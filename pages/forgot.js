603e62272206f6b4513a7d9b6935be2fbd4f774d
Sahil pillania <sahilpillania88@gmail.com>
2022-12-19 11:33:18 +0530

    Login system Design

--- pages/forgot.js
+++ pages/forgot.js
@@ -1,7 +1,8 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { BsKey } from "react-icons/bs";
 
-const Login = () => {
+const forgot = () => {
   return (
     <div>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -13,7 +14,7 @@ const Login = () => {
               alt="Your Company"
             />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Log in to your account
+              Forgot your password
             </h2>
             <p className="mt-2 mx-2 text-center text-sm ">
               Or
@@ -38,11 +39,11 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
+                  className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
                   placeholder="Email address"
                 />
               </div>
-              <div>
+              {/* <div>
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
@@ -55,33 +56,7 @@ const Login = () => {
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
                   placeholder="Password"
                 />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-purple-600 focus:ring-purple-500"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm">
-                <Link
-                  href={"/forgot"}
-                  className="font-medium text-purple-600 hover:text-purple-500"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
+              </div> */}
             </div>
 
             <div>
@@ -90,21 +65,9 @@ const Login = () => {
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-purple-600 py-2 px-4 text-sm font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-purple-500 group-hover:text-purple-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <BsKey className="text-2xl" />
                 </span>
-                Log in
+                Send OTP
               </button>
             </div>
           </htmlForm>
@@ -114,4 +77,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default forgot;
